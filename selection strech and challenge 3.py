deb1ebd4da82d304ecdd9e9c1a465a1204cde8f4
40535 <40535@PC004149.coll.lr.local>
2014-10-02 14:43:01 +0100

    selection stretch and challenge 3

---
+++ selection strech and challenge 3.py
@@ -0,0 +1,49 @@
+#Matthew Wadkin
+#01/10/14
+#this program will change the date in the normal formt
+
+date = str(input("Please enter a date in the format 12 05 03 (must be between 1931 and 2030): "))
+day = int(date[0] + date[1])
+month = int(date[3] + date[4])
+year = int(date[6] + date[7])
+
+if day == 1:
+    day = str(day) + "st"
+elif day == 2:
+    day = str(day) + "nd"
+elif day == 3:
+    day = str(day) + "rd"
+else:
+    day = str(day) + "th"
+
+if month == 1:
+    month = "January"
+elif month == 2:
+    month = "Febuary"
+elif month == 3:
+    month = "March"
+elif month == 4:
+    month = "April"
+elif month == 5:
+    month = "May"
+elif month == 6:
+    month = "June"
+elif month == 7:
+    month = "July"
+elif month == 8:
+    month = "August"
+elif month == 9:
+    month = "September"
+elif month == 10:
+    month = "October"
+elif month == 11:
+    month = "November"
+else:
+    month = "December"
+
+if year > 30:
+    year = "19" + str(year)
+else:
+    year = "20" + str(year)
+
+print("The date you entered is: {0} {1} {2}".format(day,month,year))
